41e7bcc58a7b8e9e7f4c672e28e28ad21996c000
kounenn93 <kounenn93@gmail.com>
2024-12-09 11:20:22 +0800

    整合後端；註冊功能暫結。

---
+++ FullDemo/src/main/java/com/tiamtshai/fulldemo/FullDemoApplication.java
@@ -0,0 +1,13 @@
+package com.tiamtshai.fulldemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FullDemoApplication.class, args);
+    }
+
+}
---
+++ FullDemo/src/main/java/com/tiamtshai/fulldemo/config/AppConfig.java
@@ -0,0 +1,19 @@
+package com.tiamtshai.fulldemo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class AppConfig {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
+
+/*
+@Configuration 是專門用來標註配置類的，其內部所有的 @Bean 方法會被 Spring 容器進行代理和管理，確保同一個 Bean 實例只會被創建一次（單例）。
+雖然 @Component 加 @Bean 也能達到目的，但它並不是標準做法，會降低代碼的可讀性。
+ */
